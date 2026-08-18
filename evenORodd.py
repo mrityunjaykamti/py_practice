01e91fa7c7085a8c9f9d6986af46a1d10256f604
mrityunjaykamti <mrityunjaykamti@gmail.com>
2026-08-18 23:08:20 +0530

    Even or Odd

---
+++ evenORodd.py
@@ -0,0 +1,8 @@
+def checkEvenorOdd():
+    if(num%2==0):
+        print(f"{num} is an Even number")
+    else:
+        print(f"{num} is a odd number")
+
+num=int(input("Enter number : "))
+checkEvenorOdd()
