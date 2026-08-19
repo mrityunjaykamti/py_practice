87aa0ecb64bc766699bd37a41f9f66212e9408e2
mrityunjaykamti <mrityunjaykamti@gmail.com>
2026-08-19 10:45:22 +0530

    check_prime

---
+++ check_Prime.py
@@ -0,0 +1,20 @@
+
+def check_prime():
+    count=0
+    if(num<2):
+        print(f"{num} is not a prime number")
+    else:
+        for i in range(2, num):
+            if(num%i==0):
+                count+=1
+                break
+
+        if(count==0):
+            print(f"{num} is a prime number")
+        else:
+            print(f"{num} is not a prime number")
+            print(num,"this is not a prime number")
+
+
+num=int(input("Enter a number : "))
+check_prime()
