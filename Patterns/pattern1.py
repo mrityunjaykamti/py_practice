81dfc8e4330171990dab8aa703c6948c451aea2b
Mrityunjay Kamti <mrityunjaykamti@gmail.com>
2026-08-30 00:05:17 +0530

    some changes

---
+++ Patterns/pattern1.py
@@ -0,0 +1,13 @@
+# print this pattern
+# ****
+# ****
+# ****
+# ****
+
+
+num=(int(input("Enter num : ")))
+
+for i in range(num):
+    for j in range(num):
+        print(" *",end="")
+    print("")
